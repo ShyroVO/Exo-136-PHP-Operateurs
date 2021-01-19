96c57d26a85ae9fc141d7c58ad536a8fa70a2608
ShyroVO <bosquet.laurie@outlook.com>
2021-01-19 09:42:10 +0100

    1.0

--- index.php
+++ index.php
@@ -1,26 +1,33 @@
 <?php
 
 // Premiere ligne
-echo 10 5;
+    echo 10 * 5;
 
 //Deuxieme ligne
-echo 10 2;
+    echo 10 / 2;
 
 //Troisieme ligne
-if($a $b) {
-    echo "<br><br>a est identique à b";
-}
+    $a = 1;
+    $b = 1;
+
+    if($a === $b) {
+        echo "<br><br>a est identique à b";
+    }
 
 //Quatrieme ligne
-if($a $b) {
-    echo"<br><br>a n'est pas identique à b";
-}
+    $a = 1;
+    $b = 3;
+
+    if($a !== $b) {
+        echo"<br><br>a n'est pas identique à b";
+    }
 
 
 //Cinquieme ligne
-$arr1 = ["machin", "truc"];
-$arr2 = ["truc", "machin"];
-if($arr1 $arr2) {
-    echo "<br><br>les tableaux ont le même contenu";
-}
+    $arr1 = ["machin", "truc"];
+    $arr2 = ["truc", "machin"];
+    if($arr1 === $arr2) {
+        echo "<br><br>les tableaux ont le même contenu";
+    }
 
+?>
